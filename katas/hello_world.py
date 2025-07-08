522096e11b46aaca1e1bae92a45fd825009da431
Yosef Shawah <your.email@example.com>
2025-07-08 10:23:52 +0300

    hello world solution

--- katas/hello_world.py
+++ katas/hello_world.py
@@ -6,6 +6,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    result_str = hello_world()
-    print(result_str)   # hello world should be printed
+    result_string = hello_world()
+    print(result_string)   # hello world should be printed
 
--- katas/test/test_hello_world.py
+++ katas/test/test_hello_world.py
@@ -4,4 +4,5 @@ from katas.hello_world import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
+        #test
         self.assertEqual(hello_world(), "hello world")
