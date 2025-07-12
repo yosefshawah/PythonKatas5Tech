d1cfbe3f07c67bf5fee86e4a8655ce8ef88cd644
shawah Yosef <shawahyosef@shawahs-Mac-mini.local>
2025-07-12 14:55:08 +0300

    test matrix rotation

--- katas/test/test_matrix_rotation.py
+++ katas/test/test_matrix_rotation.py
@@ -51,23 +51,5 @@ class TestRotateMatrix(unittest.TestCase):
         rotate_matrix(matrix)
         self.assertEqual(matrix, expected)
 
-    def test_5x5_matrix(self):
-        matrix = [
-            [ 1,  2,  3,  4,  5],
-            [ 6,  7,  8,  9, 10],
-            [11, 12, 13, 14, 15],
-            [16, 17, 18, 19, 20],
-            [21, 22, 23, 24, 25]
-        ]
-        expected = [
-            [21, 16, 11,  6, 1],
-            [22, 17, 12,  7, 2],
-            [23, 18, 13,  8, 3],
-            [24, 19, 14,  9, 4],
-            [25, 20, 15, 10, 5]
-        ]
-        rotate_matrix(matrix)
-        self.assertEqual(matrix, expected)
-
 if __name__ == '__main__':
     unittest.main()
