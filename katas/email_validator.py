a92d738da77ffe334d730b8c261d2cc4f3cbfa98
Yosef shawah <yosefshawah643@gmail.com>
2025-07-23 13:33:52 +0300

    sol for email validator

--- katas/email_validator.py
+++ katas/email_validator.py
@@ -21,7 +21,31 @@ def is_valid_email(email: str):
     Returns:
         True if the email is valid, False otherwise
     """
-    return False
+    
+    """
+    ^(?![._%+-])           # can't start with special chars
+    [a-zA-Z0-9._%+-]{1,64} # allowed chars in local part, length limit
+    (?<![._%+-])           # can't end with special chars
+    @
+    (?!.*\.\.)             # reject double dots in domain
+    [a-zA-Z0-9.-]{1,253}   # valid domain name characters
+    \.[a-zA-Z]{2,}$        # must end in .com, .org, etc
+
+    """
+    pattern = re.compile(
+        r"^(?![._%+-])"                  # local part must not start with special chars
+        r"[a-zA-Z0-9._%+-]{1,64}"        # local part
+        r"(?<![._%+-])"                  # local part must not end with special chars
+        r"@"
+        r"(?!.*\.\.)"                    # no double dots in domain
+        r"[a-zA-Z0-9.-]{1,253}"          # domain part
+        r"\.[a-zA-Z]{2,}$"              # domain TLD
+    )
+    
+    return bool(email and pattern.match(email))
+    
+    
+
 
 
 if __name__ == "__main__":
